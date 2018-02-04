df7b065cd15074b8a0a4161465c07432b471191c
srbgd <alkkoloto@yandex.ru>
2018-02-04 21:02:31 +0300

    add new database

--- database.py
+++ database.py
@@ -4,16 +4,14 @@ import json
 
 class DataBase:
 
-	
-	db = None
+	db = []
 	file = 'db.json'
 
-	def __init__(self, file):
-		self.file = file + '.json'
+	def __init__(self):
 		if os.path.isfile(self.file):
-			self.db = json.loads(open(self.file).read())
-		else:
-			self.db = []
+			s = open(self.file).read()
+			if s != '':
+				self.db = json.loads(s)
 
 	def update(self):
 		json.dump(self.db, open(self.file, 'w'))
@@ -22,16 +20,26 @@ class DataBase:
 		self.db.append(item)
 		self.update()
 
-	def lookup(self, attributes):
-		return [item for item in self.db if all(value == item['attributes'][key] for key, value in attributes.items())]
+	def lookup(self, type, attributes):
+		return [i for i in self.db if type == i['type'] or type == '' and all(v == i['attributes'][k] for k, v in attributes.items())]
 
 	def delete(self, id):
+		size = len(self.db)
 		self.db = [item for item in self.db if item['id'] != id]
 		self.update()
+		if len(self.db) != size:
+			return True
+		else:
+			return False
 
 	def modify(self, id, attributes):
 		for i in self.db:
 			if i['id'] == id:
 				for key, value in attributes.items():
 					i['attributes'][key] = value
-		self.update()
+				self.update()
+				return True
+		return False
+
+	def get_max_id(self):
+		return max(i['id'] for i in self.db) if self.db else -1
